457358f5406a081529c5e16fe41cf901752e02f3
VN DEV <minh.tu@novasto.co.jp>
2022-08-08 19:34:33 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="text-3xl text-red-400">
+    Hello World
+  </div>
+</template>
+
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+export {}
+</script>
